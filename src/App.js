dc55dbe9c7fb0bd18f308e854fddcc302a0b8d7b
coryp4 <cory@burtano.com>
2020-12-11 10:13:02 -0800

    half way there

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Characters from './components/Character'
+import axios from 'axios'
+
+const BASE_URL = 'https://swapi.py4e.com/api/people/'
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -9,10 +13,28 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const [characterSet, setCharacterSet] = useState(null)
+
+  useEffect(() => {
+    axios.get(BASE_URL)
+    .then(res => {
+      setCharacterSet(res.data.results);
+    })
+    .catch(err => {
+      console.log('oops!')
+    })
+
+  }, [])
+
+  console.log(characterSet)
+
   return (
+    <>
     <div className="App">
       <h1 className="Header">Characters</h1>
     </div>
+    <Characters/>
+    </>
   );
 }
 
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,17 @@
 // Write your Character component here
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+
+// const BASE_URL = 'https://swapi.py4e.com/api/people/'
+
+export default function Characters() {
+
+    return(
+        <div>
+            <h2></h2>
+            <p></p>
+        </div>
+    )
+    
+}
